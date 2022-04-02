f6d6af66033d8fe104146f1de36a0e5be25b57a1
Gero <54119666+gerson-henrique@users.noreply.github.com>
2022-04-02 17:06:06 -0300

    Update and rename kata_12_root_calculator.js to Sum of Digits | Digital Root.js

---
+++ Sum of Digits | Digital Root.js
@@ -0,0 +1,24 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+  let digits = n;
+  let sum = 0;
+    
+  while (digits.toString().length > 1) {
+    sum = 0;
+    digits = digits.toString().split('').map(Number);
+    for (let i = 0 ; i < digits.length ; i++ ) {
+      sum  += digits[i];
+    }
+    digits = sum;
+  }
+  return sum
+}
