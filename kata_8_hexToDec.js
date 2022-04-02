cdc58cb76ef923027e2d2620a03d69a2914c0401
Gero <54119666+gerson-henrique@users.noreply.github.com>
2022-04-02 16:52:41 -0300

    Update and rename kata_8_hexToDec to kata_8_hexToDec.js

--- kata_8_hexToDec.js
+++ kata_8_hexToDec.js
@@ -1,3 +1,10 @@
+// Write a program that will convert a hexadecimal number, represented as a string (e.g. "10af8c"), to its decimal equivalent.
+
+// The program should return -1 for invalid hexadecimal strings.
+
+// Source
+// All of Computer Science http://www.wolframalpha.com/examples/NumberBases.html
+
 function hexToDec(hexadecimal) {
  let spell= hexadecimal.toString()
     spell = spell.split('')
